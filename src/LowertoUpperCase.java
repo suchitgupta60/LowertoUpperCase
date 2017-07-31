f5e37772a078ff52078ab5847a596ae8b1d93c5d
Suchit Gupta <suchitgupta60@gmail.com>
2017-07-31 07:34:06 -0400

    use this code as a supplement

---
+++ src/LowertoUpperCase.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class LowertoUpperCase {
+
+	public static void main(String[] args) 
+	{ 
+
+		// Create a scanner keyboard to accept input from the keyboard.
+
+		Scanner scan = new Scanner(System.in);
+
+		// Ask user to type lower case alphabet.
+
+		System.out.println("This program converts lower Case letters to Upper Case Letter.");
+		System.out.println("Enter your String in lowercase : ");           
+		
+		for(int i=0;i<5;i++){
+			  System.out.println());
+			  }
+		
+		// Read in user entry.
+		//String string = scan.nextLine( );
+
+		// Output the result.
+		//System.out.println("The String now in uppercase:\n"  +  string.toUpperCase());
+	}
+	public static String[] ch = new String[100];
+}
